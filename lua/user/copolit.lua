be1462d5850ff688b33447de609b507e17a0dd18
huangtao <542248377@qq.com>
2023-05-05 22:06:02 +0800

    copilot会输出奇怪字符的问题修复

--- lua/user/copolit.lua
+++ lua/user/copolit.lua
@@ -1,2 +1,3 @@
-vim.keymap.set('i', '<M-CR>', 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true, script = true })
+vim.keymap.set('i', '<M-CR>', 'copilot#Accept("<CR>")',
+    { expr = true, noremap = true, silent = true, script = true, replace_keycodes = false })
 vim.g.copilot_no_tab_map = true
