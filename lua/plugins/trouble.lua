6aba2ecaab9034e4b2437282149e011fa32adf11
heaven <542248377@qq.com>
2023-05-19 14:12:01 +0800

    给异常添加样式

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -7,5 +7,11 @@ return {
         require("trouble").setup()
 
         vim.keymap.set("n", "<leader>e", ":TroubleToggle<cr>")
+
+        local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
     end,
 }
