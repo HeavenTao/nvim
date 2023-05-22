8ab54fc86248d1d01b23b3a890441ba70568ae0b
heaven <542248377@qq.com>
2023-05-22 13:47:14 +0800

    自动保存

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,5 +3,29 @@ return {
     config = function()
         require("lsp.lua").setup()
         require("lsp.python").setup()
+
+        vim.api.nvim_create_autocmd('LspAttach',
+            {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    --vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+                    vim.api.nvim_create_autocmd('BufWritePre',
+                        {
+                            buffer = ev.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = ev.buf })
+                            end
+                        }
+                    )
+                end
+            }
+        )
     end
 }
