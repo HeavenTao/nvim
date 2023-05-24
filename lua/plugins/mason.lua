5535983c52ced63c4946604a42bec114938452f2
heaven <542248377@qq.com>
2023-05-24 09:26:56 +0800

    重新调整语言服务

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,18 @@
 return {
     "williamboman/mason.nvim",
-    build=":MasonUpdate",
-    dependencies={
-        {"williamboman/mason-lspconfig.nvim"}
+    build = ":MasonUpdate",
+    dependencies = {
+        { "williamboman/mason-lspconfig.nvim" }
     },
-    config=function()
+    config = function()
         require("mason").setup()
-        require("mason-lspconfig").setup()
+        require("mason-lspconfig").setup {
+            ensure_installed = {
+                "html",
+                "jsonls",
+                "lua_ls",
+                "pyright"
+            }
+        }
     end
 }
