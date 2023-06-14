73ad4fd36ce0ce608961f97b614101f6d8419d01
huangtao <542248377@qq.com>
2023-06-14 21:31:28 +0800

    change theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,19 +1,14 @@
 return {
     "folke/tokyonight.nvim",
     lazy = false,
+    enabled = true,
     config = function()
         --主題設置
         if vim.fn.has('termguicolors') ~= 0 then
             vim.o.termguicolors = true
         end
-        vim.o.background = 'dark'
-        vim.g.everforest_background = 'soft'
-        vim.g.everforest_better_performance = 1
-        --vim.cmd.colorscheme('everforest')
-        vim.cmd.colorscheme('tokyonight')
-
+        vim.cmd.colorscheme('tokyonight-moon')
         require('tokyonight').setup({
-            style = 'night'
         })
     end
 }
