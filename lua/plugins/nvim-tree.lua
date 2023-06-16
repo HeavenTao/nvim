0ca962af60e116d2a7dc606d4db9c76f4dd82ff4
heaven <542248377@qq.com>
2023-06-16 18:23:19 +0800

    remove coc and change tree plugin

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        vim.opt.termguicolors = true
+        require("nvim-tree").setup()
+    end,
+    keys = {
+        { "<leader>n", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" }
+    }
+}
