dd71bb5024536167477299ed21c938a1def57a98
huangtao <542248377@qq.com>
2023-06-17 22:30:58 +0800

    fix python lsp to find root_dir

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,5 +1,5 @@
 return {
-    setup=function()
+    setup = function()
         --python
         local root_files = {
             'pyproject.toml',
@@ -10,18 +10,23 @@ return {
             'Pipfile',
             'pyrightconfig.json',
         }
-        --遇到没有存根的库,pyright速度会变慢,所以需要指定存根路径 
-        local root_file= vim.fs.find(root_files, { upward = true })[1]
+        --遇到没有存根的库,pyright速度会变慢,所以需要指定存根路径
+        local root_file = vim.fs.find(root_files, { upward = true })[1]
         local stubsPath = nil
+        local root_path = ""
         if root_file then
-            stubsPath= vim.fs.dirname(root_file).."/stubs"
+            root_path = vim.fs.dirname(root_file)
+            stubsPath = root_path .. "/stubs"
         end
-        require("lspconfig").pyright.setup{
-            settings={
-                python={
-                    analysis={
-                        stubPath=stubsPath,
-                        diagnosticMode="openFilesOnly"
+        require("lspconfig").pyright.setup {
+            root_dir = function()
+                return root_path
+            end,
+            settings = {
+                python = {
+                    analysis = {
+                        stubPath = stubsPath,
+                        diagnosticMode = "openFilesOnly"
                     }
                 }
             }
