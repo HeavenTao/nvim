9817123b3dad1af29cc972ab96f3ea4919fc3d14
huangtao <542248377@qq.com>
2023-07-07 23:12:27 +0800

    add todo plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,11 +5,11 @@ return {
     --lazy = false,
     config = function()
     end,
-    keys={
-        {"<leader>f","<Cmd>Telescope find_files<cr>",desc="find_files"},
-        {"<leader>fg","<Cmd>Telescope live_grep<cr>",desc="live_grep"},
-        {"<leader>fh","<Cmd>Telescope search_history<cr>",desc="search_history"},
-        {"<leader>fk","<Cmd>Telescope keymaps<cr>",desc="keymaps"},
+    keys = {
+        { "<leader>f",  "<Cmd>Telescope find_files<cr>", desc = "find_files" },
+        { "<leader>fg", "<Cmd>Telescope live_grep<cr>",  desc = "live_grep" },
+        { "<leader>ft", "<Cmd>TodoTelescope<cr>",        desc = "todo" },
+        { "<leader>fk", "<Cmd>Telescope keymaps<cr>",    desc = "keymaps" },
     }
 
 }
