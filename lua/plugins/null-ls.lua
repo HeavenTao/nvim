0026228bc8ef84633f229b59c556c114074ff537
heaven <542248377@qq.com>
2023-07-26 23:19:24 +0800

    add nix formatter

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,6 +8,7 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.nixfmt,
             }
         })
     end
