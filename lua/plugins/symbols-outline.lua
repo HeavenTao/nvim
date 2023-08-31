6b1b7bba6c9e67aa56e080121d883a86f6e0aaf5
heaven <542248377@qq.com>
2023-08-31 13:09:03 +0800

    add shortkey symbolsoutline

--- lua/plugins/symbols-outline.lua
+++ lua/plugins/symbols-outline.lua
@@ -3,6 +3,8 @@ return {
     config = function()
         require("symbols-outline").setup()
     end,
-    cmd = "SymbolsOutline"
+    cmd = "SymbolsOutline",
+    keys = {
+        { "<leader>s", "<Cmd>SymbolsOutline<cr>", desc = "SymbolsOutline" },
+    }
 }
-
