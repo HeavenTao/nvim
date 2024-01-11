4f959eb4d1310ee280670c46d381bd21764da614
HeavenTao <542248377@qq.com>
2024-01-11 14:10:55 +0800

    1.treesj延迟加载

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,6 +1,7 @@
 return {
     "Wansmer/treesj",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
+    keys = { "<space>m", "<space>j", "<spance>s" },
     config = function()
         require("treesj").setup({})
     end
