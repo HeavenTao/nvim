618a6f36c3557adbdc5577e52f5a5c827774cb63
HeavenTao <542248377@qq.com>
2024-01-15 14:43:52 +0800

    dev

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,17 +1,31 @@
 return {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
     'hrsh7th/nvim-cmp',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
+    dependencies={
+	    'neovim/nvim-lspconfig',
+	    'hrsh7th/cmp-nvim-lsp',
+	    'hrsh7th/cmp-buffer',
+	    'hrsh7th/cmp-path',
+	    'hrsh7th/cmp-cmdline',
+	    'L3MON4D3/LuaSnip',
+	    'saadparwaiz1/cmp_luasnip',
+	    'L3MON4D3/LuaSnip',
+	    'saadparwaiz1/cmp_luasnip'
+    },
     config=function()
         -- Set up nvim-cmp.
+	local has_words_before = function()
+		unpack = unpack or table.unpack
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	end
+ 	local luasnip= require('luasnip')
         local cmp = require'cmp'
-        
         cmp.setup({
+		experimental={
+			ghost_text={
+				hl_group="CmpGhostText"
+			}
+		},
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
@@ -25,13 +39,32 @@ return {
                 --completion = cmp.config.window.bordered(),
                 --documentation = cmp.config.window.bordered(),
             },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-            }),
+            mapping ={
+
+		    ["<Tab>"] = cmp.mapping(function(fallback)
+			    if cmp.visible() then
+				    cmp.select_next_item()
+				    -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+				    -- that way you will only jump inside the snippet region
+			    elseif luasnip.expand_or_jumpable() then
+				    luasnip.expand_or_jump()
+			    elseif has_words_before() then
+				    cmp.complete()
+			    else
+				    fallback()
+			    end
+		    end, { "i", "s" }),
+
+		    ["<S-Tab>"] = cmp.mapping(function(fallback)
+			    if cmp.visible() then
+				    cmp.select_prev_item()
+			    elseif luasnip.jumpable(-1) then
+				    luasnip.jump(-1)
+			    else
+				    fallback()
+			    end
+		    end, { "i", "s" }),
+	    } ,
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 --{ name = 'vsnip' }, -- For vsnip users.
@@ -70,5 +103,11 @@ return {
                 { name = 'cmdline' }
             })
         })
-    end
+	-- Set up lspconfig.
+	local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+	require('lspconfig').lua_ls.setup {
+		capabilities = capabilities
+	}
+end
 }
