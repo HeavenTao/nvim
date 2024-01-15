a3864f68d642cfc354bb6aa963eefaac11ec4f3c
HeavenTao <542248377@qq.com>
2024-01-15 17:29:16 +0800

    1.调整lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,19 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    --lazy = false,
-    config = function()
-        require('lualine').setup {
-            options = {
-                section_separators = { left = '', right = '' },
-                sections = {
-                    lualine_a = { 'mode' },
-                    lualine_b = { 'branch', 'diff', 'diagnostics' },
-                    lualine_c = { 'filename' },
-                    lualine_x = { 'encoding', 'filetype' },
-                    lualine_y = { 'progress' },
-                    lualine_z = { 'location' }
-                }
-            }
-        }
-    end
+	"nvim-lualine/lualine.nvim",
+	--lazy = false,
+	config = function()
+		require("lualine").setup({
+			options = {
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
