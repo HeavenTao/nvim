c899c9c47d9ac547b64daf50cfedb61bd8087d8f
HeavenTao <542248377@qq.com>
2024-01-15 22:33:37 +0800

    1.修改lualine样式

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,15 +4,23 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
 			},
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
 				lualine_x = { "encoding", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
+				lualine_y = { "progress", "location" },
+				lualine_z = {
+					{
+						function()
+							return " " .. os.date("%R")
+						end,
+						color = { bg = "#9ece6a" },
+					},
+				},
 			},
 		})
 	end,
