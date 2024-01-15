37b408d577cdfd860ba6ebedc8bdedd31eb1e8f0
HeavenTao <542248377@qq.com>
2024-01-15 16:28:01 +0800

    1.telescope 支持打开多个文件”

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,14 @@
-vim.g.mapleader = "," -- 功能键
-vim.g.maplocalleader = "\\" --第二功能键
-vim.opt.number = true --显示行号
-vim.opt.relativenumber = true --相对行号
+vim.g.mapleader = ","                       -- 功能键
+vim.g.maplocalleader = "\\"                 --第二功能键
+vim.opt.number = true                       --显示行号
+vim.opt.relativenumber = true               --相对行号
 vim.opt.clipboard:append({ "unnamedplus" }) --+寄存器成为默认使用的寄存器
-vim.opt.number = true --显示行号
-vim.opt.hlsearch = true --高亮查找
-vim.opt.incsearch = true --增量查找
-vim.opt.ignorecase = true --查找忽略大小写
-vim.opt.timeoutlen = 300 --按键间隔
-vim.opt.shiftwidth = 4 --
+vim.opt.number = true                       --显示行号
+vim.opt.hlsearch = true                     --高亮查找
+vim.opt.incsearch = true                    --增量查找
+vim.opt.ignorecase = true                   --查找忽略大小写
+vim.opt.timeoutlen = 300                    --按键间隔
+vim.opt.shiftwidth = 4                      --
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
 vim.opt.autoindent = true
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,12 +2,38 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { { "nvim-lua/plenary.nvim" } },
 	config = function()
+		local custom_actions = {}
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
+
+		function custom_actions.fzf_multi_select(prompt_bufnr)
+			local picker = action_state.get_current_picker(prompt_bufnr)
+			local num_selections = #(picker:get_multi_selection())
+
+			if num_selections > 1 then
+				picker = action_state.get_current_picker(prompt_bufnr)
+				for _, entry in ipairs(picker:get_multi_selection()) do
+					vim.cmd(string.format("%s %s", ":e!", entry.value))
+				end
+				vim.cmd("stopinsert")
+			else
+				actions.file_edit(prompt_bufnr)
+			end
+		end
 		require("telescope").setup({
 			defaults = {
 				layout_strategy = "horizontal",
 				layout_config = {
 					width = 0.95,
 					height = 0.95,
+				},
+				mappings = {
+					i = {
+						["<cr>"] = custom_actions.fzf_multi_select,
+					},
+					n = {
+						["<cr>"] = custom_actions.fzf_multi_select,
+					},
 				},
 			},
 		})
