1e100a467092517728e5c357ae818ec9fa8f2406
HeavenTao <542248377@qq.com>
2024-01-16 09:31:48 +0800

    1.调整诊断的图标

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,14 @@
-vim.g.mapleader = ","                       -- 功能键
-vim.g.maplocalleader = "\\"                 --第二功能键
-vim.opt.number = true                       --显示行号
-vim.opt.relativenumber = true               --相对行号
+vim.g.mapleader = "," -- 功能键
+vim.g.maplocalleader = "\\" --第二功能键
+vim.opt.number = true --显示行号
+vim.opt.relativenumber = true --相对行号
 vim.opt.clipboard:append({ "unnamedplus" }) --+寄存器成为默认使用的寄存器
-vim.opt.number = true                       --显示行号
-vim.opt.hlsearch = true                     --高亮查找
-vim.opt.incsearch = true                    --增量查找
-vim.opt.ignorecase = true                   --查找忽略大小写
-vim.opt.timeoutlen = 300                    --按键间隔
-vim.opt.shiftwidth = 4                      --
+vim.opt.number = true --显示行号
+vim.opt.hlsearch = true --高亮查找
+vim.opt.incsearch = true --增量查找
+vim.opt.ignorecase = true --查找忽略大小写
+vim.opt.timeoutlen = 300 --按键间隔
+vim.opt.shiftwidth = 4 --
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
 vim.opt.autoindent = true
@@ -18,3 +18,9 @@ vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 10
 vim.opt.termguicolors = true
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,12 +5,6 @@ return {
 	},
 	config = function()
 		require("trouble").setup()
-
-		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
 	end,
 	keys = {
 		{ "<leader>e", "<Cmd>TroubleToggle<CR>", desc = "Trouble" },
