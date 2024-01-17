90ea6a65cbdd3eee1cab99e70f553dd53bb14aea
HeavenTao <542248377@qq.com>
2024-01-17 13:23:18 +0800

    1.调整comment反向不生效的问题

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,9 @@
 return {
-    "preservim/nerdcommenter",
-    config = function()
-        vim.g.NERDCreateDefaultMappings = 0
-    end,
-    keys = {
-        { "<leader>cc", mode = { "n", "v" }, "<Plug>NERDCommenterInvert", desc = "Commenter" }
-    }
+	"preservim/nerdcommenter",
+	init = function()
+		vim.g.NERDCreateDefaultMappings = 0
+	end,
+	keys = {
+		{ "<leader>cc", mode = { "n", "v" }, "<Plug>NERDCommenterInvert", desc = "Commenter" },
+	},
 }
