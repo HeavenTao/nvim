e28cb871a964d923dfe3c779357dcc1456b3a031
HeavenTao <542248377@qq.com>
2024-01-17 13:50:59 +0800

    1.调整neotree显示

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,39 +1,39 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    },
-    keys = {
-        { "<leader>n", "<cmd>Neotree source=filesystem position=float toggle=true reveal=true<cr>", "Neotree toggle" }
-    },
-    config = function()
-        vim.fn.sign_define("DiagnosticSignError",
-            { text = " ", texthl = "DiagnosticSignError" })
-        vim.fn.sign_define("DiagnosticSignWarn",
-            { text = " ", texthl = "DiagnosticSignWarn" })
-        vim.fn.sign_define("DiagnosticSignInfo",
-            { text = " ", texthl = "DiagnosticSignInfo" })
-        vim.fn.sign_define("DiagnosticSignHint",
-            { text = "󰌵", texthl = "DiagnosticSignHint" })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	keys = {
+		{
+			"<leader>n",
+			"<cmd>Neotree source=filesystem position=right toggle=true reveal=true<cr>",
+			"Neotree toggle",
+		},
+	},
+	config = function()
+		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-        require("neo-tree").setup({
-            close_if_last_window = true,
-            filesystem = {
-                use_libuv_file_watcher = false,
-                window = {
-                    mappings = {
-                        ["l"] = {
-                            "toggle_node"
-                        },
-                        ["h"] = {
-                            "close_node"
-                        },
-                    }
-                }
-            },
-        })
-    end
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				use_libuv_file_watcher = false,
+				window = {
+					mappings = {
+						["l"] = {
+							"toggle_node",
+						},
+						["h"] = {
+							"close_node",
+						},
+					},
+				},
+			},
+		})
+	end,
 }
