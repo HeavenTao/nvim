3afd5f5afbed5e9e13ea6c141fdc0971afc93cd4
542248377 <P@$$w0rd>
2024-02-19 11:38:59 +0800

    add markdown-preview

--- lua/plugins/translator.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"voldikss/vim-translator",
-	enabled = false,
-	config = function() end,
-	keys = {
-		{ "<leader>t", "<Plug>Translate", desc = "Translate" },
-		{ "<leader>t", "<Plug>TranslateV", mode = "v" },
-	},
-}
