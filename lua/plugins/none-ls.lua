892053abbee165335663f59e6091a497fee683d9
HeavenTao <542248377@qq.com>
2024-02-25 11:59:46 +0800

    1.sync

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,15 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                --python
-                null_ls.builtins.formatting.black,
-                --bash
-                null_ls.builtins.formatting.beautysh
-            }
-        })
-    end
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				--python
+				null_ls.builtins.formatting.black,
+			},
+		})
+	end,
 }
