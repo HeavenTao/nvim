e6f9be56f193d27e5eb3a6c2e0c657960aafd50f
HeavenTao <542248377@qq.com>
2024-03-12 10:03:31 +0800

    1.add some textobjects

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -1,26 +1,65 @@
 return {
 	"nvim-treesitter/nvim-treesitter-textobjects",
-	enable = false,
 	config = function()
-		-- When in diff mode, we want to use the default
-		-- vim text objects c & C instead of the treesitter ones.
-		local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-		local configs = require("nvim-treesitter.configs")
-		for name, fn in pairs(move) do
-			if name:find("goto") == 1 then
-				move[name] = function(q, ...)
-					if vim.wo.diff then
-						local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-						for key, query in pairs(config or {}) do
-							if q == query and key:find("[%]%[][cC]") then
-								vim.cmd("normal! " .. key)
-								return
-							end
-						end
-					end
-					return fn(q, ...)
-				end
-			end
-		end
+		require("nvim-treesitter.configs").setup({
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+					},
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+					include_surrounding_whitespace = true,
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = { query = "@class.outer", desc = "Next class start" },
+					--
+					-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+					["]o"] = "@loop.*",
+					-- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+					--
+					-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+					-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+					["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+					["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+				-- Below will go to either the start or the end, whichever is closer.
+				-- Use if you want more granular movements
+				-- Make it even more gradual by adding multiple queries and regex.
+				goto_next = {
+					["]d"] = "@conditional.outer",
+				},
+				goto_previous = {
+					["[d"] = "@conditional.outer",
+				},
+			},
+		})
 	end,
 }
