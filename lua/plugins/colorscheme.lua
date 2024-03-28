46230296fafb9a3c1556dd6a1ee1c3c6a82a6224
HeavenTao <542248377@qq.com>
2024-03-28 09:11:24 +0800

    1.change theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-	--"folke/tokyonight.nvim",
-	--config=function()
-	--vim.cmd.colorscheme("tokyonight-night")
-	--end
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
+	"folke/tokyonight.nvim",
 	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-		})
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
+	--"catppuccin/nvim",
+	--name = "catppuccin",
+	--priority = 1000,
+	--config = function()
+	--require("catppuccin").setup({
+	--flavour = "mocha",
+	--})
+	--vim.cmd.colorscheme("catppuccin")
+	--end,
 }
