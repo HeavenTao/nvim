1a5b02554a9e6d81a10e23796e003614eb90f186
HeavenTao <542248377@qq.com>
2024-04-26 22:02:44 +0800

    1.change startup

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,31 +1,31 @@
 return {
-    "glepnir/dashboard-nvim",
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-    event = 'VimEnter',
-    config = function()
-        require('dashboard').setup {
-            shortcut_type = "number", --  shorcut type 'letter' or 'number'
-            config = {
-                week_header = {
-                    enable = true,
-                    concat = " Welcome HeavenTao!"
-                },
-                --shortcut = {
-                --{
-                --desc = " Tree",
-                --group = 'string',
-                --icon = ""
-                --},
-                --},
-                project = {
-                    enable = false,
-                    limit = 5,
-                    icon = 'your icon',
-                    label = '',
-                    action = 'Telescope find_files cwd='
-                },
-                mru = { enable = false, limit = 10, icon = '󰣇', label = '', },
-            }
-        }
-    end
+	"nvimdev/dashboard-nvim",
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			shortcut_type = "number", --  shorcut type 'letter' or 'number'
+			config = {
+				week_header = {
+					enable = true,
+					concat = " Welcome HeavenTao!",
+				},
+				--shortcut = {
+				--{
+				--desc = " Tree",
+				--group = 'string',
+				--icon = ""
+				--},
+				--},
+				project = {
+					enable = false,
+					limit = 5,
+					icon = "your icon",
+					label = "",
+					action = "Telescope find_files cwd=",
+				},
+				mru = { enable = false, limit = 10, icon = "󰣇", label = "" },
+			},
+		})
+	end,
 }
