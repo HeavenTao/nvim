19a363ebf734575d1411a96549cb0c762733faf5
HeavenTao <542248377@qq.com>
2024-04-30 23:16:47 +0800

    1.which key add icon

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,12 +1,18 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        plugins = { spelling = true },
-    },
-    config = function(_, opts)
-        local wk = require("which-key")
-        wk.setup(opts)
-        wk.register(opts.defaults)
-    end,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		plugins = { spelling = true },
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup({
+			icons = {
+				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+				separator = "", -- symbol used between a key and it's label
+				group = "+",
+			},
+		})
+		wk.register(opts.defaults)
+	end,
 }
