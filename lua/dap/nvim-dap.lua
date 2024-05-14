7adb672174c3705d31441ada06299857b577c143
HeavenTao <542248377@qq.com>
2024-05-14 13:06:44 +0800

    1.add c,c++,rust debugger

--- lua/dap/nvim-dap.lua
+++ lua/dap/nvim-dap.lua
@@ -15,6 +15,34 @@ return {
 			"DapStopped",
 			{ text = "", texthl = "DapBreakpoint", linehl = "DapStoppedLinehl", numhl = "" }
 		)
+
+		--c C++ rust
+		local dap = require("dap")
+		dap.adapters.codelldb = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+				args = { "--port", "${port}" },
+			},
+		}
+
+		local languages = { "c", "cpp", "rust" }
+
+		for _, lang in ipairs(languages) do
+			dap.configurations[lang] = {
+				{
+					name = "Launch file",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable:", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+				},
+			}
+		end
 	end,
 	keys = {
 		{
