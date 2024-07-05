84b99665e241d4edfe3bd0a9f24065375628f433
HeavenTao <542248377@qq.com>
2024-07-05 21:10:36 +0800

    sync

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,3 +1,4 @@
 vim.g.neovide_hide_mouse_when_typing = false
 vim.g.neovide_cursor_trail_size = 0
-vim.o.guifont = "FiraCode Nerd Font:h12"
+vim.o.guifont = "FiraCode Nerd Font:h14"
+vim.g.frame = "none"
