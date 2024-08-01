c707e82f91dd48d837cc7ea064782ffc4af1f7a7
HeavenTao <542248377@qq.com>
2024-08-01 21:15:44 +0800

    1.add textobject

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -1,23 +1,26 @@
 return {
 	"echasnovski/mini.ai",
-	enabled = false,
 	event = "VeryLazy",
-	opts = function()
+	config = function()
 		local ai = require("mini.ai")
-		return {
+		ai.setup({
 			n_lines = 500,
 			custom_textobjects = {
-				o = ai.gen_spec.treesitter({
+				o = ai.gen_spec.treesitter({ -- code block
 					a = { "@block.outer", "@conditional.outer", "@loop.outer" },
 					i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-				}, {}),
-				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-				t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+				}),
+				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+				t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+				d = { "%f[%d]%d+" }, -- digits
+				e = { -- word with case
+					{ "%u[%l%d]+%f[^%l%d]", "%f[%s][%l%d]+%f[^%l%d]", "%f[%p][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+					"^().*()$",
+				},
+				u = ai.gen_spec.function_call(), -- u for "usage"
+				U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
 			},
-		}
-	end,
-	config = function(_, opts)
-		require("mini.ai").setup(opts)
+		})
 	end,
 }
