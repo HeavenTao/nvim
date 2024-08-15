8ebd639219463c2ecaef00d3e127e4d9570a57f6
HeavenTao <542248377@qq.com>
2024-08-15 21:48:21 +0800

    add default neovide

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+if vim.g.neovide then
+  vim.g.neovide_cursor_trail_size=0
+  vim.g.neovide_cursor_animate_command_line=false
+end
