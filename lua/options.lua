c0f3ab2070196594d4048cfe8e54e6aaa31c3a42
HeavenTao <542248377@qq.com>
2024-08-15 23:02:31 +0800

    add some key

--- lua/options.lua
+++ lua/options.lua
@@ -1,10 +1,11 @@
 require "nvchad.options"
 
+vim.g.mapleader = ","
+vim.o.timeoutlen = 300
 -- add yours here!
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 if vim.g.neovide then
-  vim.g.neovide_cursor_trail_size=0
-  vim.g.neovide_cursor_animate_command_line=false
+  vim.g.neovide_cursor_trail_size = 0
 end
