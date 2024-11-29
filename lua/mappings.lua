3ccacbf66fd33b3fe77ce492253ba3459d77d21f
HeavenTao <542248377@qq.com>
2024-11-29 15:20:00 +0800

    1.重新调整映射

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,6 +11,7 @@ map("n", "<A-h>", "<C-W>h", { desc = "Window Left" })
 map("n", "<A-j>", "<C-W>j", { desc = "Window Down" })
 map("n", "<A-k>", "<C-W>k", { desc = "Window Up" })
 
+--edit move
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "move end of line" })
 map("i", "<C-h>", "<Left>", { desc = "move left" })
@@ -18,31 +19,21 @@ map("i", "<C-l>", "<Right>", { desc = "move right" })
 map("i", "<C-j>", "<Down>", { desc = "move down" })
 map("i", "<C-k>", "<Up>", { desc = "move up" })
 
+--Clear highlights
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
 
+--Save all
 map("n", "<leader>ww", "<cmd>wall<CR>", { desc = "General Save file" })
--- map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "General Copy whole file" })
---
--- map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "Toggle line number" })
--- map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle relative number" })
--- map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "Toggle nvcheatsheet" })
+
+--NvCheatsheet
+map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "Toggle nvcheatsheet" })
+
+--Format
 map("n", "<leader>fm", function()
   require("conform").format { lsp_fallback = true }
 end, { desc = "General Format file" })
 
--- global lsp mappings
-map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic loclist" })
-
--- tabufline
--- map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
-
--- map("n", "<tab>", function()
---   require("nvchad.tabufline").next()
--- end, { desc = "buffer goto next" })
---
--- map("n", "<S-tab>", function()
---   require("nvchad.tabufline").prev()
--- end, { desc = "buffer goto prev" })
+--Tabline
 map("n", "<A-,>", function()
   require("nvchad.tabufline").prev()
 end, { desc = "Tab Left" })
@@ -69,26 +60,19 @@ map("v", "<leader>cc", "gc", { desc = "Toggle comment", remap = true })
 
 -- nvimtree
 map("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
--- map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
 
 -- telescope
 map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
 map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
 map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
--- map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
 -- map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
 -- map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
-map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+--map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
 -- map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
-map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
+--map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
 map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "telescope nvchad themes" })
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
-map(
-  "n",
-  "<leader>fa",
-  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-  { desc = "telescope find all files" }
-)
 
 -- terminal
 map("t", "<C-q>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
@@ -143,11 +127,11 @@ map("t", "<ESC>", "<C-\\><C-N>", { desc = "terminal exit terminal mode" })
 --code runner
 map("n", "<F5>", function()
   require("nvchad.term").runner {
-    id="htoggleTerm",
+    id = "htoggleTerm",
     pos = "sp",
     cmd = function()
       local file = vim.fn.expand "%"
-      local outfile= "./debug/" .. vim.fn.expand("%:t:r")
+      local outfile = "./debug/" .. vim.fn.expand "%:t:r"
       local ft_cmds = {
         c = "clang -o " .. outfile .. " -g " .. file .. " && " .. "./" .. outfile,
       }
