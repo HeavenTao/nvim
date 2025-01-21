771f4df0e8695a1f9f123ca9f1ae55917054ab25
HeavenTao <542248377@qq.com>
2025-01-21 13:53:08 +0800

    1.sync

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -15,14 +15,14 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
---vim.cmd("autocmd BufNewFile,BufRead * set fileformat=unix")
+vim.cmd "autocmd BufNewFile,BufRead * set fileformat=unix"
 
 --clipboard on wsl
-if vim.fn.has "wsl" == 1 then
-  vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
-    callback = function()
-      vim.fn.system("clip.exe", vim.fn.getreg '"')
-    end,
-  })
-end
+-- if vim.fn.has "wsl" == 1 then
+--   vim.api.nvim_create_autocmd("TextYankPost", {
+--     group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+--     callback = function()
+--       vim.fn.system("clip.exe", vim.fn.getreg '"')
+--     end,
+--   })
+-- end
