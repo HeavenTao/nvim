5796bb01b511780f3cb90524cc0aba8db675bc3f
HeavenTao <542248377@qq.com>
2025-02-07 06:23:30 +0000

    1.优化lspconfig的配置，同时兼容arch和nixos

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -18,45 +18,46 @@ for _, lsp in ipairs(servers) do
 end
 
 local os_name = utils.get_os_name()
-
+local vue_language_server_path = ""
 if os_name == "nixos" then
+  local user = os.getenv "USER"
+  vue_language_server_path = "/etc/profiles/per-user/" .. user .. "/bin/vue-language-server"
 else
   local mason_registry = require "mason-registry"
-  local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+  vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
     .. "/node_modules/@vue/language-server"
-
-  lspconfig.ts_ls.setup {
-    init_options = {
-      plugins = {
-        {
-          name = "@vue/typescript-plugin",
-          location = vue_language_server_path,
-          languages = { "vue" },
-        },
-      },
-    },
-    settings = {
-      javascript = {
-        format = {
-          indentSize = 2,
-          tabSize = 2,
-          convertTabsToSpaces = true,
-          semicolons = "insert",
-        },
-      },
-      typescript = {
-        format = {
-          indentSize = 2,
-          tabSize = 2,
-          convertTabsToSpaces = true,
-          semicolons = "insert",
-        },
-      },
-    },
-    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-  lspconfig.volar.setup {}
 end
+
+lspconfig.ts_ls.setup {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+      },
+    },
+  },
+  settings = {
+    javascript = {
+      format = {
+        indentSize = 2,
+        tabSize = 2,
+        convertTabsToSpaces = true,
+        semicolons = "insert",
+      },
+    },
+    typescript = {
+      format = {
+        indentSize = 2,
+        tabSize = 2,
+        convertTabsToSpaces = true,
+        semicolons = "insert",
+      },
+    },
+  },
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
