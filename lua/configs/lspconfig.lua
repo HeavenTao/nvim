dc6c80811869d14b07291d0203e78be1c8d4b361
HeavenTao <542248377@qq.com>
2025-05-21 17:28:02 +0800

    1.add nix lsp nil 2.sync

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -9,30 +9,31 @@ vim.lsp.enable "jsonls"
 vim.lsp.enable "ts_ls"
 
 --nixd
-vim.lsp.config["nixd"] = {
-  cmd = { "nixd" },
-  settings = {
-    nixd = {
-      nixpkgs = {
-        expr = "import <nixpkgs> { }",
-      },
-      formatting = {
-        command = { "nixfmt" },
-      },
-      options = {
-        nixos = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
-        },
-        home_manager = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
-        },
-      },
-    },
-  },
-  filetypes = { "nix" },
-  root_markers = { "flake.nix", ".git" },
-}
-vim.lsp.enable "nixd"
+-- vim.lsp.config["nixd"] = {
+--   cmd = { "nixd" },
+--   settings = {
+--     nixd = {
+--       nixpkgs = {
+--         expr = "import <nixpkgs> { }",
+--       },
+--       formatting = {
+--         command = { "nixfmt" },
+--       },
+--       options = {
+--         nixos = {
+--           expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+--         },
+--         home_manager = {
+--           expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+--         },
+--       },
+--     },
+--   },
+--   filetypes = { "nix" },
+--   root_markers = { "flake.nix", ".git" },
+-- }
+-- vim.lsp.enable "nixd"
+vim.lsp.enable "nil_ls"
 
 --zig
 local user = os.getenv "USER"
