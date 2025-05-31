aeb8230c18a482f8d27d12dcbb4434a80ff9a5d5
HeavenTao <542248377@qq.com>
2025-05-31 17:32:54 +0800

    1.change flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -16,9 +16,15 @@ return {
   },
   keys = {
     {
+      "/",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump { search = { forward = true, wrap = true, multi_window = false } }
+      end,
+    },
+    {
       "s",
       mode = { "n", "x", "o" },
-
       function()
         -- default options: exact mode, multi window, all directions, with a backdrop
         require("flash").jump()
@@ -33,6 +39,14 @@ return {
         require("flash").treesitter()
       end,
       desc = "Flash Treesitter",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
     },
   },
 }
