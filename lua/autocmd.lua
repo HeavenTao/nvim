bd70bd80fd474e15c8079593655f8f1f8568c60c
HeavenTao <542248377@qq.com>
2026-01-05 10:49:29 +0800

    1.use qwen

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -117,7 +117,7 @@ vim.api.nvim_create_user_command("ToggleAIChat", function(args)
       border = "rounded",
     },
     cmd = function()
-      return "iflow"
+      return "qwen"
     end,
   }
 end, { desc = "ToggleAIChat" })
