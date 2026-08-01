acbbbb64c6559fb8d2ee8070c3d740055bee86bd
HeavenTao <542248377@qq.com>
2026-08-01 14:13:34 +0800

    1.disabled preview treesitter in telescope

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,15 +1,8 @@
 return {
   defaults = {
-    mappings = {
-      i = {
-        ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
-      },
-      n = {
-        ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
-      },
-    },
     preview = {
       hide_on_startup = true,
+      treesitter = false,
     },
     file_ignore_patterns = { "^obj/", "^bin/" },
   },
